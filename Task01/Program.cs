dd7a546e3bf36fd6262b38120d25b660865f74af
dimonakus <dimonakus@gmail.com>
2023-04-08 23:14:10 +0500

    Well done Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -10,9 +10,10 @@ Console.WriteLine("Введите Первое целое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите Второе целое число");
 int number2 = Convert.ToInt32(Console.ReadLine());
+//int number2 = Math.Mod(number2);
 Console.WriteLine("А теперь я проверю, является ли первое число квадратом второго");
-double square_root = Math.Sqrt(number1);
-if (square_root == number2)
+//double square_root = Math.Sqrt(number1);
+if (number2*number2 == number1)
 {
     Console.WriteLine($"Да, число {number1} является квадратом числа {number2}");
 }
