660e489f5f056aab0adb2ef3ff6db51a8fb84b95
dimonakus <dimonakus@gmail.com>
2023-04-09 18:01:17 +0500

    Task 08 well done

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -2,3 +2,11 @@
 //принимает число (N), а на выходе показывает все чётные
 //числа от 1 до N.
 
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= numberA)
+{
+    if (count % 2 == 0) Console.Write(count + ", ");
+    count++;
+}
