4dc87d686ec06a8be85a71bfeb92aad8adcf46f6
dimonakus <dimonakus@gmail.com>
2023-04-11 17:37:24 +0500

    Task 11, 14, 16 well done

---
+++ Task11/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(99, 1000); //99 + 1
+Console.WriteLine($"Случайное число из отрезка 99 - 1000 -> {number}");
+
+
+// int maxDigit (int num) // num = number
+// {
+//     int firstDigit = num / 100;
+// int secondDigit = num % 10;
+
+// int maxDigit = firstDigit > secondDigit ? firstDigit : firstDigit;
+// return maxDigit;
+// }
+
---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = MultipleTwoDigits(number,5,10);
+Console.WriteLine(result ? "да" : "нет");
+bool MultipleTwoDigits(int num, int num1,int num2)
+{
+   return num % num1==0 && num % num2==0;
+}
