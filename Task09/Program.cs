7b4e48c9a75c09b4980210468dbf4577f84976db
dimonakus <dimonakus@gmail.com>
2023-04-11 17:39:36 +0500

    Task 09 well done

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,14 +5,27 @@
 // 12-> 2
 // 85 -> 8
 
+
+
 int number = new Random().Next(10, 100); //99 + 1
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
 
 // if(firstDigit > secondDigit) Console.WriteLine($"Наибальшая цифра числа {number} -> {firstDigit}");
 // else 
 // Console.WriteLine($"Наибальшая цифра числа {number} -> {secondDigit}");
 
-int maxDigit = firstDigit > secondDigit ? firstDigit : firstDigit; // ? - тогда, : - иначе
-Console.WriteLine($"Наибальшая цифра числа {number} -> {maxDigit}");
+//int maxDigit = firstDigit > secondDigit ? firstDigit : firstDigit; // ? - тогда, : - иначе
+//Console.WriteLine($"Наибальшая цифра числа {number} -> {maxDigit}");
+
+int maxDigit2 = maxDigit(number);
+Console.WriteLine($"Наибальшая цифра числа {number} -> {maxDigit2}");
+
+int maxDigit (int num) // num = number
+{
+    int firstDigit = num / 10;
+int secondDigit = num % 10;
+int maxDigit = firstDigit > secondDigit ? firstDigit : firstDigit;
+return maxDigit;
+}
