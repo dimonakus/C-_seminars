b453de18a82412904155f4d1ab0444efd840e8dd
dimonakus <dimonakus@gmail.com>
2023-04-12 10:48:56 +0500

    Task14 well done

--- Task14/Program.cs
+++ Task14/Program.cs
@@ -8,9 +8,11 @@
 
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-bool result = MultipleTwoDigits(number,5,10);
+
+bool result = MultipleTwoDigits(number, 7, 23);
 Console.WriteLine(result ? "да" : "нет");
-bool MultipleTwoDigits(int num, int num1,int num2)
+
+bool MultipleTwoDigits(int num, int num1, int num2)
 {
-   return num % num1==0 && num % num2==0;
+    return num % num1 == 0 && num % num2 == 0;
 }
