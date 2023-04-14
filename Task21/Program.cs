cb62e5bbe55710ba7b00050f5c5d67319248dc6c
dimonakus <dimonakus@gmail.com>
2023-04-14 18:18:24 +0500

    Well done Task21, 23

---
+++ Task21/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координаты X первой точки X1: ");
+int x1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Y первой точки Y1: ");
+int y1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Z первой точки Z1: ");
+int z1Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты X второй точки X2: ");
+int x2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Y второй точки Y2: ");
+int y2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координаты Z второй точки Z2: ");
+int z2Coordinate = Convert.ToInt32(Console.ReadLine());
+
+
+double distance = Distance(x1Coordinate, y1Coordinate, x2Coordinate, y2Coordinate, z1Coordinate, x2Coordinate);
+double distanceRound = Math.Round(distance, 2, MidpointRounding.ToZero);
+Console.WriteLine($"Длина отрезка -> {distanceRound}");
+
+
+
+double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+}
---
+++ Task23/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Pow3(number);
+
+
+
+void Pow3 (int num)
+{
+int count = 1;
+while (count<=num)
+{
+    //Console.Write(count+" ");
+    Console.WriteLine($"{count, 3} {Math.Pow(count, 3), 4}");
+    count++;
+}
+}
