a170ff69e8806a78c5364f855b853f32ccc1a697
dimonakus <dimonakus@gmail.com>
2023-04-19 19:07:39 +0500

    Tasks 25, 27 well done

---
+++ Task25/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+//и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Write("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int powNumbers = PowN(numberA, numberB);
+Console.WriteLine($"{numberA}, {numberB} -> {powNumbers}");
+
+int PowN(int numA, int numB)
+{
+    int powN = 0;
+    int temp = numA;
+    for (int i = 1; i <= numB - 1; i++)
+    {
+        checked // Проверка на переполнение
+        {
+
+            powN = temp * numA;
+            temp = powN;
+        }
+    }
+    return powN;
+}
---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+//и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите целое положительное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigits = SumDigits(number);
+
+Console.WriteLine($"Сумма цифр в числе: {number} -> {sumDigits}");
+
+int SumDigits(int num)
+{
+    int count = 0;
+    while (num > 0)
+    {
+        count = count + num % 10 - 1;
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
