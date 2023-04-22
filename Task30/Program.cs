95dc231c99407530044b59f688d0b968d4afb14c
dimonakus <dimonakus@gmail.com>
2023-04-22 06:55:47 +0500

    Task30 add  correct

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -9,20 +9,17 @@ ArrayPrint(array);
 
 void ArrayIn(int[] array)
 {
-   //int length = array.Length;
    var random = new Random();
    for (int i = 0; i < array.Length; i++)
    {
-      array[i] = random.Next(0, 2);
+      array[i] = random.Next(0, 100);
    }
 }
 
 void ArrayPrint(int[] array)
 {
-   int length = array.Length;
-   for (int i = 0; i < length; i++)
+   for (int i = 0; i < array.Length; i++)
    {
-      
-      Console.Write($"{array[i]} ");
+            Console.Write($"{array[i]} ");
    }
 }
