942d62de6b8da586d6bb36c49acb7e18f1827468
dimonakus <dimonakus@gmail.com>
2023-05-25 07:33:17 +0500

    Tasks 64-69 added

--- Task67/Program.cs
+++ Task67/Program.cs
@@ -1,1 +1,29 @@
-﻿
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число
+//и возвращать сумму его цифр
+//453 -> 12
+//45 -> 9
+
+// int FactorialRec(int n)
+// {
+// if (n == 1) return 1;
+// return n * FactorialRec(n - 1);
+// }
+
+// Console.WriteLine(FactorialRec(10)); // 3628800
+
+Console.WriteLine("Введите целое натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigits = SumDigits(number);
+Console.WriteLine(sumDigits);
+
+int SumDigits(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + SumDigits(num / 10);
+    //Таже самая запись через тернарный оператор
+    //return num == 0 ? 0 : num % 10 + SumDigits(num / 10);
+}
+
+
+
--- Task69/Program.cs
+++ Task69/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿//Задача 69: Напишите программу, которая на вход принимает два числа А и B,
+// и возводит число А в целую степень B с помощью рекурсии.
+//А = 3; B = 5 -> 243 (3^5)
+//A = 2; B = 3 -> 8
+
+
+Console.WriteLine("Введите первое целое число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое натуральное число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int degree = Degree(numberA, numberB);
+Console.WriteLine(degree);
+
+int Degree(int numA, int numB)
+{
+    if (numB == 0) return 1;
+    return numA*Degree(numA, numB-1);
+}
